774b3f7cfa4da1ed83d4e403486d82b26002ff92
JakariaShrabon <jakariabhuiyanshrabon@gmail.com>
2023-09-13 14:05:38 +0600

    ll

---
+++ Pettern-34.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+    int r, c;
+    int n;
+    printf("Enter Your Number : ");
+    scanf("%d", &n);
+    for (r = 1; r <= n; r++)
+    {
+
+        for (c = 1; c <= r; c++)
+        {
+            printf("%d", r / 2);
+        }
+        printf("\n");
+    }
+    for (r = n - 1; r >= 1; r--)
+    {
+
+        for (c = 1; c <= r; c++)
+        {
+            printf("%d", r / 2);
+        }
+        printf("\n");
+    }
+}
