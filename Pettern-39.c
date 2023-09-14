36fa142c0dff5716ea9a61bd8812fed8a412de23
Jakaria_Shrabon <133669270+JakariaShrabon@users.noreply.github.com>
2023-09-15 01:37:24 +0600

    Add files via upload
    ll

---
+++ Pettern-39.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int r, c, n;
+    printf("Enter Your Number :");
+    scanf("%d", &n);
+    for (r = 1; r <= n; r++)
+    {
+        for (c = 1; c <= n; c++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
